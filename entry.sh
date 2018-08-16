c6e7b141f9b642387688c569fab00f34e466d42c
Zachary Claret-Scott <zachary.claretscott@probablyrational.com>
2018-08-16 10:27:45 +0100

    Better entry

--- entry.sh
+++ entry.sh
@@ -1,7 +1,22 @@
-git config --global user.name "$GIT_NAME" &&
-git config --global user.email "$GIT_EMAIL" &&
-service ssh start &&
-service apache2 start &&
-monit &&
-[ "${GIT_PULL}" ] && rm -rf ..?* .[!.]* * && git clone $GIT_PULL . && /root/.composer/vendor/bin/composer install && chmod 777 /var/www -R && php artisan config:cache || clear &&
-clear
+#!/bin/bash
+git config --global user.name "$GIT_NAME"
+git config --global user.email "$GIT_EMAIL"
+service ssh start
+service apache2 start
+monit
+if [ "$GIT_PULL" != "" ]; then
+	if [ ! -f /var/www/.git/config ]; then
+		rm -rf ..?* .[!.]* *
+		git clone $GIT_PULL .
+		/root/.composer/vendor/bin/composer install
+		chmod 777 /var/www -R
+		php artisan config:cache
+	else
+		git reset --hard HEAD
+		git clean -f -d
+		git pull;
+		/root/.composer/vendor/bin/composer install
+		chmod 777 /var/www -R
+		php artisan config:cache
+	fi
+fi
